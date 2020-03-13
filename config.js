6bcf8d1e4e090f0c7cd0e5e8f0ea2d9abf608bec
ZhangYuanlin <bhzhangyuanlin@163.com>
2020-03-14 00:43:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -44,13 +44,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
+        turn_on: "请点击一下开始~",
+        play: "要开始播放音乐了哦~",
+        bannar_coming: "再点一下下啦~",
+        balloons_flying: "好像少了点啥~",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "A short letter for you~",
     }
 };
